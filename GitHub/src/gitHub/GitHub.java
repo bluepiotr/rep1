ee4d2a29392815493f8e2f5f9d4662984c1f8c41
rgomez02 <rgomez02@CSVPC02.dcs.bbk.ac.uk>
2012-10-27 11:36:20 +0100

    First Commit

---
+++ GitHub/src/gitHub/GitHub.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gitHub;
+
+/**
+ * @author rgomez02
+ *
+ */
+public class GitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+
+	}
+
+}
